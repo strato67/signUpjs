99c1669450de0b5e2e395ce89afb12f51a1cf448
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-09-08 21:08:20 -0400

    added sign up form

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <div className="navbar bg-primary text-primary-content">
+            <a className="btn btn-ghost normal-case text-xl">Legit Company</a>
+        </div>
+    );
+}
+
+export default Navbar;
